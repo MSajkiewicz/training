7cf1f3ba39047f7913b0cd9a6ceda18fa180677e
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2020-02-16 11:48:47 +0100

    Cleanup

--- src/main/java/com/training/training/codewarsTraining/oddOrEven/OddOrEvenSolution2.java
+++
@@ -1,9 +0,0 @@
-package com.training.training.codewarsTraining.oddOrEven;
-
-import static java.util.Arrays.stream;
-
-public class OddOrEvenSolution2 {
-    public static String oddOrEven (int[] array) {
-    return stream(array).sum() % 2 == 0 ? "even" : "odd";
-    }
-}
